562c957d547c6210a201043081261f0f93e2577e
Yayun Eldina <yayuneldina02@gmail.com>
2024-05-14 11:40:30 +0700

    Jobsheet 12_Method Move

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -19,9 +19,12 @@ class FileUploadController extends Controller
             //$namaFile = $request->berkas->getClientOriginalName();
             $extfile = $request->berkas->getClientOriginalName();
             $namaFile = 'web-'.time().".".$extfile;
-            $path = $request->berkas->storeAs('public', $namaFile);
 
-            $partBaru = asset('storage/'.$namaFile);
+            $path = $request->berkas->move('gambar', $namaFile);
+            $path = str_replace("\\","//", $path);
+            echo "Variabel path berisi $path <br>";
+
+            $partBaru = asset('gambar/'.$namaFile);
             echo "proses upload berhasil, file berada di: $path";
             echo "<br>";
             echo "Tampilan link:<a href='$partBaru'>$partBaru</a>";
